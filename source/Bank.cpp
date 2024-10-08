bc3610c46b2518ca4f2abf921b66fe1289cdfaa9
KonstantinDobr <snastink@yandex.ru>
2024-10-08 20:12:15 +0300

    Bank fix

--- source/Bank.cpp
+++ source/Bank.cpp
@@ -8,7 +8,7 @@
 #include <cstdint>
 
 Bank::Bank(std::uint16_t max_size, bank_time::Time* time, std::uint16_t bankers_number)
-: queue_(*new ClientsQueue(max_size)), time_(time), bankers_number_(bankers_number), total_earn_(0), new_client_probability_(1.0)
+: queue_(ClientsQueue(max_size)), time_(time), bankers_number_(bankers_number), total_earn_(0), new_client_probability_(1.0)
 {
 }
 
